9f2248f406c20ba84478d22fb42a23dd3c0307d3
Mike Burns <maddogmikeb@users.noreply.github.com>
2020-03-27 10:07:03 +1000

    Consistency

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,11 +4,13 @@ Param(
 )
 function Show-Completed {
     param(
+        [Parameter(Mandatory = $true)]
         [double]$Percentage,
-        [string]$Title = "",
+        [Parameter(Mandatory = $true)]
+        [string]$Activity,
         [string]$Status = "Processing"
     )
-    Write-Progress -Activity $Title -Status $Status -percentComplete $Percentage
+    Write-Progress -Activity $Activity -Status $Status -percentComplete $Percentage
     Write-Host "`r$([math]::floor($Percentage).ToString().PadLeft(3, " "))% Completed" -NoNewline
 }
 
@@ -18,7 +20,7 @@ Write-Host "Building " -NoNewline
 Write-Host "$($manifest.name) $($manifest.version) " -ForegroundColor "Green" -NoNewline
 Write-Host "($(if ($IsReleaseBuild) { "Release" } else { "Debug" }))" -ForegroundColor "DarkMagenta"
 
-Show-Completed -Percentage 1 -Title "Build" -Status "Creating Folders"
+Show-Completed -Percentage 1 -Activity "Build" -Status "Creating Folders"
 
 if (!(Test-Path -Path './obj/')) {
     New-Item -ItemType directory -Path './obj/'
@@ -31,36 +33,36 @@ if (!(Test-Path -Path './obj/debug')) {
 }
 
 if ($IsReleaseBuild) {
-    Show-Completed -Percentage 5 -Title "Cleaning"
-    Get-ChildItem -Path './obj/release' -Include * -File -Recurse | ForEach-Object { $_.Delete() }
+    Show-Completed -Percentage 5 -Activity "Cleaning"
+    Get-ChildItem -Path './obj/release' -Include * -File -Recurse | ForEach-Object { Set-ItemProperty $_.FullName -name IsReadOnly -value $false; $_.Delete() }
 
-    Show-Completed -Percentage 10 -Title "Copying"
+    Show-Completed -Percentage 10 -Activity "Copying"
     Copy-Item -Path './src/*' -Destination './obj/release/' -Force -Recurse
 
     $i = 0; $files = Get-ChildItem './obj/release/' -Recurse -Force -Include *.css
     $files | ForEach-Object {
         & "./scripts/Minify-CSS.ps1" -file $_
-        Show-Completed -Percentage (10 + (($i++ / ($files.Count) * 100) * 0.4)) -Title "Minifying" -Status "Minifying $($_.BaseName).$($_.Extension)"
+        Show-Completed -Percentage (10 + (($i++ / ($files.Count) * 100) * 0.4)) -Activity "Minifying" -Status "Minifying $($_.BaseName).$($_.Extension)"
     }
 
     $i = 0;  $files = Get-ChildItem './obj/release/' -Recurse -Force -Include *.js
     $files | ForEach-Object {
         & "./scripts/Minify-Javascript.ps1" -file $_
-        Show-Completed -Percentage (50 + (($i++ / ($files.Count) * 100) * 0.4)) -Title "Minifying" -Status "Minifying $($_.BaseName).$($_.Extension)"
+        Show-Completed -Percentage (50 + (($i++ / ($files.Count) * 100) * 0.4)) -Activity "Minifying" -Status "Minifying $($_.BaseName).$($_.Extension)"
     }
 
-    Show-Completed -Percentage 90 -Title "Archiving"
+    Show-Completed -Percentage 90 -Activity "Archiving"
     Compress-Archive -Path "./obj/release/*" -DestinationPath "./obj/release/$($manifest.name).zip" -Force
 
-    Show-Completed -Percentage 95 -Title "Cleaning"
+    Show-Completed -Percentage 95 -Activity "Cleaning"
     Remove-Item "./obj/release/*" -Exclude "$($manifest.name).zip" -Force -Recurse
 }
 else {
-    Show-Completed -Percentage 33 -Title "Cleaning"
-    Get-ChildItem -Path './obj/debug' -Include * -File -Recurse | ForEach-Object { $_.Delete() }
+    Show-Completed -Percentage 33 -Activity "Cleaning"
+    Get-ChildItem -Path './obj/debug' -Include * -File -Recurse | ForEach-Object { Set-ItemProperty $_.FullName -name IsReadOnly -value $false; $_.Delete() }
 
-    Show-Completed -Percentage 66 -Title "Copying"
+    Show-Completed -Percentage 66 -Activity "Copying"
     Copy-Item -Path './src/*' -Destination './obj/debug/' -Force -Recurse
 }
 
-Show-Completed -Percentage 100 -Title "Build" -Status "Completed"
+Show-Completed -Percentage 100 -Activity "Build" -Status "Completed"
